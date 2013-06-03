a44f33c7dbc7393b4b3d19e51d35f32786955fed
John Metta <john@johnmetta.com>
2013-06-03 11:55:51 -0700

    Add a bunch of new stuff and make my own

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -72,7 +72,7 @@ $(function(){
     skip_forks: githubInfo.data('github-skip-forks'),
     target: githubInfo
   });
-
+  
   var $container = $('#post-container');
   $container.imagesLoaded(function(){
     $container.masonry({
